5b58df34517e10d580f42b3d0775e724b359d447
Bartosz Jurga <barti.jurga@gmail.com>
2017-03-04 00:49:32 +0100

    members sorting prepared

--- client/components/App.js
+++ client/components/App.js
@@ -9,7 +9,6 @@ export default class App extends React.Component {
             userRepos: []
         };
         this.getUsers();
-        this.getUserRepos();
     }
 
     getUserRepos (username) {
@@ -32,6 +31,23 @@ export default class App extends React.Component {
                 }.bind(this));
     }
 
+    sortMembers (members) {
+        const sort_key = "points";
+        members.map(member => member[sort_key] =
+            parseInt(member.public_repos) + parseInt(member.gists) + parseInt(member.followers));
+        return this.sortMethod(members, sort_key)
+    }
+
+    sortMethod (arrayOfObjects, key) {
+        return arrayOfObjects.sort(function (a, b) {
+            if (a[key] < b[key]) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+    }
+
     componentWillUnmount () {
         this.serverRequest.abort();
     }
@@ -39,6 +55,8 @@ export default class App extends React.Component {
     render () {
         const usersData = this.state.usersData;
         if (JSON.stringify(usersData) !== '[]') {
+            const array = this.sortMembers(usersData);
+            console.log('array: ', array);
             return (
                 <div>
                     <h1>Members!</h1>
