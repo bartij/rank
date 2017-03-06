26cae2c270703bfb25f2adbca8d1d2be94c356ed
Bartosz Jurga <barti.jurga@gmail.com>
2017-03-06 21:50:34 +0100

    css loader installed and loading view added

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import RankingList from './RankingList';
-import style from './Styles/App.css';
 
 export default class App extends React.Component {
     constructor(props) {
